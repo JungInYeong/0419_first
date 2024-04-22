7a063e5120adaa624ad268c2dca0064234fa3b71
JungInYeong <167291569+JungInYeong@users.noreply.github.com>
2024-04-22 20:04:45 +0900

    Create 0422_m3.cpp

---
+++ 0419_first/0422_m3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "나이를 입력하세요 : ";
+	cin >> n;
+
+	if (1 <= n && n<= 7)
+	{
+		cout << "유아";
+	}
+	else if (8 <= n  && n <= 13)
+	{
+		cout << "초등학생";
+	}
+	else if (14 <= n && n <= 16)
+	{
+		cout << "중학생";
+	}
+	else if (n <= 17 && n <= 19)
+	{
+		cout << "고등학생";
+	}
+	else if(20 >= n)
+	{
+		cout << "성인";
+	}
+
+	return 0;
+}
